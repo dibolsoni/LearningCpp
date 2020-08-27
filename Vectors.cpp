3ab893253cb3011daab599f01539f491929200d4
beca <dibolsoni@gmail.com>
2020-08-27 08:59:03 -0300

    basic vectors

---
+++ Vectors.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+
+namespace Vectors 
+{
+    static int called{0};
+    void printLength(const std::vector<int>& array)
+    {
+        std::cout << "Called:" << called++ << ". The length is: " << array.size() << '\n';
+    }
+    
+    // vectors works as arrays but owns its memory management.
+    // so it is more dynamic
+    void basic()
+    {
+        // as with std::array, the type can be omitted since C++17
+        std::vector array { 9, 7, 5, 3, 1 }; // deduced to std::vector<int, 5>
+        array = { 9, 8, 7 }; // okay, array length is now 3
+        printLength(array);
+        array = { 0, 1, 2, 3, 4 }; // okay, array length is now 5
+        printLength(array);
+        array.resize(3); // okay, array length is now 3
+        printLength(array);
+      
+    }
+
+
+    void caller()
+    {
+        std::cout << "vectors!" << "\n";
+
+        basic();
+    }
+}
