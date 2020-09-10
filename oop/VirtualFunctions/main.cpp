c7b61aee67e7c88deaafeebc4d97c9c2dd44cbb4
beca <dibolsoni@gmail.com>
2020-09-10 15:18:56 -0300

    Virtual class

--- oop/VirtualFunctions/main.cpp
+++ oop/VirtualFunctions/main.cpp
@@ -1,20 +1,28 @@
-#include "Cow.h"
-#include "Cat.h"
-#include "Dog.h"
-
+// #include "Cow.h"
+// #include "Cat.h"
+// #include "Dog.h"
+// #include "VirtualClass.cpp"
+#include "WithoutVirtualClass.cpp"
 #include <iostream>
 
 
+// int main()
+// {
+//     Cow cow{"Betsy"};
+//     std::cout << cow.getName() << " says " << cow.speak() << '\n';
+
+//     Cat cat{"Cat"};
+//     std::cout << cat.getName() << " says " << cat.speak() << '\n';
+
+//     Dog dog{"Dog"};
+//     std::cout << dog.getName() << " says " << dog.speak() << '\n';
+
+//     return 0;
+// }
+
 int main()
 {
-    Cow cow{"Betsy"};
-    std::cout << cow.getName() << " says " << cow.speak() << '\n';
-
-    Cat cat{"Cat"};
-    std::cout << cat.getName() << " says " << cat.speak() << '\n';
-
-    Dog dog{"Dog"};
-    std::cout << dog.getName() << " says " << dog.speak() << '\n';
-
+    Copier copier(1, 2, 3);
+ 
     return 0;
 }
